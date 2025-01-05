ee991f271b54c04656e78020447472922e022726
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-01-05 13:37:40 -0300

    FEAT: Criando State que sorteia e quarda a palavra usada no desafio

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import styles from "./App.module.css"
+import { useEffect, useState } from "react"
+ 
+import { WORDS, Challenge } from "./utils/words"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
@@ -8,14 +11,31 @@ import { Button } from "./components/Button"
 import { LetterUsed } from "./components/LettersUsed"
 
 export default function App() {
+  const [attempts, setAttempt] = useState(0)
+  const [letter, setLetter] = useState("")
+  const [challege, setChallenge] = useState<Challenge | null>(null)
+
   function handleRestartGame(){
     alert("Reiniciando o jogo!")
   }
 
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+    setChallenge(randomWord)
+
+    setAttempt(0)
+    setLetter("")
+  }
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
         
         <Tip tip="Uma das linguagens de programação mais utilizadas"/>
 
